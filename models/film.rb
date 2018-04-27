8e0d889c488a88c6ed7a1c656cbf64482e81bc20
Joe Stafford <joestafford1986@gmail.com>
2018-04-27 14:45:09 +0100

    refactored code

--- models/film.rb
+++ models/film.rb
@@ -12,15 +12,26 @@ class Film
   end
 
   def save()
-    sql = "INSERT INTO films (title, price) VALUES ($1, $2) RETURNING id"
+    sql = "INSERT INTO films (title, price)
+    VALUES ($1, $2)
+    RETURNING id"
     values = [@title, @price]
     film = SqlRunner.run( sql, values ).first
     @id = film['id'].to_i
   end
 
   def update()
-    sql = "UPDATE films SET (title, price) = ($1, $2) WHERE id = $3"
+    sql = "UPDATE films
+    SET (title, price) = ($1, $2)
+    WHERE id = $3"
     values = [@title, @price, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM films
+    WHERE id = $1"
+    values = [@id]
     SqlRunner.run(sql, values)
   end
 
@@ -33,6 +44,11 @@ class Film
   def self.map_items(film_data)
     result = film_data.map { |film| Film.new( film ) }
     return result
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM films"
+    SqlRunner.run(sql)
   end
 
 end
--- models/ticket.rb
+++ models/ticket.rb
@@ -14,15 +14,26 @@ class Ticket
   end
 
   def save()
-    sql = "INSERT INTO tickets (customer_id, film_id) VALUES ($1, $2) RETURNING id"
+    sql = "INSERT INTO tickets (customer_id, film_id)
+    VALUES ($1, $2)
+    RETURNING id"
     values = [@customer_id, @film_id]
     ticket = SqlRunner.run( sql,values ).first
     @id = ticket['id'].to_i
   end
 
   def update()
-    sql = "UPDATE films SET (title, price) = ($1, $2) WHERE id = $3"
-    values = [@title, @price, @id]
+    sql = "UPDATE tickets
+    SET (customer_id, film_id) = ($1, $2)
+    WHERE id = $3"
+    values = [@customer_id, @film_id, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM tickets
+    WHERE id = $1"
+    values = [@id]
     SqlRunner.run(sql, values)
   end
 
@@ -35,6 +46,11 @@ class Ticket
   def self.map_items(ticket_data)
     result = ticket_data.map { |ticket| Ticket.new( ticket ) }
     return result
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM tickets"
+    SqlRunner.run(sql)
   end
 
 end
