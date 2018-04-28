1938d54c671e739f30d0258a224775b955daad73
Joe Stafford <joestafford1986@gmail.com>
2018-04-28 12:36:31 +0100

    added save method to screening

--- models/screening.rb
+++ models/screening.rb
@@ -12,4 +12,13 @@ class Screening
     @film_id = options['film_id'].to_i
   end
 
+  def save()
+    sql = "INSERT INTO screenings (show_time, capacity, film_id)
+    VALUES ($1, $2, $3)
+    RETURNING id"
+    values = [@show_time, @capacity, @film_id]
+    screening = SqlRunner.run( sql,values ).first
+    @id = screening['id'].to_i
+  end
+
 end
