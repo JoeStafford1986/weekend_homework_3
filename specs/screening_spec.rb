c75801c8297862eb3ae52e427f5a2523a820d376
Joe Stafford <joestafford1986@gmail.com>
2018-04-29 11:04:03 +0100

    setup test for most popular screening method

--- specs/screening_spec.rb
+++ specs/screening_spec.rb
@@ -2,6 +2,10 @@ require('minitest/autorun')
 require('minitest/rg')
 
 require_relative('../models/screening')
+require_relative('../models/film')
+require_relative('../models/customer')
+require_relative('../models/ticket')
+
 
 class ScreeningTest < MiniTest::Test
 
@@ -14,24 +18,53 @@ class ScreeningTest < MiniTest::Test
       'name' => 'Joe Stafford',
       'funds' => 10
       })
+    @customer2 = Customer.new({
+      'name' => 'Joseph Stafford',
+      'funds' => 1
+      })
 
     @customer1.save()
+    @customer2.save()
 
     @film1 = Film.new({
       'title' => 'Dr Strangelove',
       'price' => 2
       })
+    @film2 = Film.new({
+      'title' => 'Dr Strangelove',
+      'price' => 2
+      })
 
     @film1.save()
+    @film2.save()
+
+    @screening1 = Screening.new({
+      'show_time' => '12:00',
+      'capacity' => 50,
+      'film_id' => @film1.id
+      })
+    @screening2 = Screening.new({
+      'show_time' => '18:00',
+      'capacity' => 100,
+      'film_id' => @film2.id
+      })
+    @screening3 = Screening.new({
+      'show_time' => '00:00',
+      'capacity' => 2,
+      'film_id' => @film1.id
+      })
+
+    @screening1.save()
+    @screening2.save()
+    @screening3.save()
+
   end
 
-  # def test_most_popular
-  #   @customer1.save()
-  #   @film1.save()
-  #   Ticket.create(@customer1,@film1)
-  #   Ticket.create(@customer2,@film1)
-  #   Ticket.create(@customer1,@film1)
-  #   assert_equal(@film1, Screening.most_popular())
-  # end
+  def test_most_popular
+    Ticket.create(@customer1,@film1, @screening1)
+    Ticket.create(@customer2,@film1, @screening1)
+    Ticket.create(@customer1,@film2, @screening2)
+    assert_equal(@film1, Screening.most_popular())
+  end
 
 end
